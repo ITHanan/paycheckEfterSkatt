df966ce1743c71fefa3343e84046a94ff76bb9ea
ITHanan <155847905+ITHanan@users.noreply.github.com>
2024-09-25 14:52:21 +0200

    Add project files.

---
+++ CalculatPaycheckSkatt.cs
@@ -0,0 +1,14 @@
+﻿
+namespace paycheckEfterSkatt
+{
+    public class CalculatPaycheckSkatt
+    {
+       public void CalculatPaycheckEfterSkatt(int paycheckInnanskatt,int skatt)
+        { 
+            double paycheckefterskatt = paycheckInnanskatt * (100 - skatt) / 100;
+            Console.WriteLine($"Hear you can see your paycheck efter skatt  {paycheckefterskatt}");
+        }
+      
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace paycheckEfterSkatt
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skapa en Console App som skriver ut Lön efter skatt (vi måste ha en klass
+            //och måste skriva in alla värde själva via terminal för att kunna kalkulera)
+
+            Console.WriteLine("Enter your Paycheck with the skatt. ");
+
+            // tagit imot värde från terminalet
+            string paycheckInnanskattsomString = Console.ReadLine()!;
+            Console.WriteLine(paycheckInnanskattsomString);
+
+            // till siffra
+            int paycheckInnanskatt = Convert.ToInt32(paycheckInnanskattsomString); 
+
+
+            Console.WriteLine("Enter skatt in  % ");
+            string skattsomString = Console.ReadLine()!;
+            Console.WriteLine(skattsomString);
+            int skatt = Convert.ToInt32(skattsomString);
+
+
+
+
+            CalculatPaycheckSkatt mycalculatPaycheckSkatt01 = new CalculatPaycheckSkatt();
+            mycalculatPaycheckSkatt01.CalculatPaycheckEfterSkatt(paycheckInnanskatt, skatt); 
+
+
+
+        }
+    }
+}
