b8673633631c6b216e9070d7de705753d9204524
ITHanan <155847905+ITHanan@users.noreply.github.com>
2024-09-25 15:08:22 +0200

    I added  comment

--- CalculatPaycheckSkatt.cs
+++ CalculatPaycheckSkatt.cs
@@ -4,7 +4,7 @@ namespace paycheckEfterSkatt
     public class CalculatPaycheckSkatt
     {
        public void CalculatPaycheckEfterSkatt(int paycheckInnanskatt,int skatt)
-        { 
+        { // here I can calculat tax 
             double paycheckefterskatt = paycheckInnanskatt * (100 - skatt) / 100;
             Console.WriteLine($"Hear you can see your paycheck efter skatt  {paycheckefterskatt}");
         }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
-﻿namespace paycheckEfterSkatt
+﻿using System;
+
+namespace paycheckEfterSkatt
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            //Skapa en Console App som skriver ut Lön efter skatt (vi måste ha en klass
-            //och måste skriva in alla värde själva via terminal för att kunna kalkulera)
+            // skriver ut Lön efter skatt 
+
+            //This line prints a message to the console asking the user to input their gross paycheck (before tax).
 
             Console.WriteLine("Enter your Paycheck with the skatt. ");
 
-            // tagit imot värde från terminalet
+
+            //Console.ReadLine() reads the user input as a string and stores it in paycheckInnanskattsomString.
+            //Console.WriteLine(paycheckInnanskattsomString) prints the entered paycheck value back to the user.
+
+           
+           // This line converts the user input(which is a string) to an integer using Convert.ToInt32().
+           // If the user enters something non-numeric, this could throw an exception.
             string paycheckInnanskattsomString = Console.ReadLine()!;
             Console.WriteLine(paycheckInnanskattsomString);
 
@@ -23,7 +32,8 @@
             int skatt = Convert.ToInt32(skattsomString);
 
 
-
+            //create a new instance of the CalculatPaycheckSkatt class. This object (mycalculatPaycheckSkatt01)
+            //is used to call the method CalculatPaycheckEfterSkatt.
 
             CalculatPaycheckSkatt mycalculatPaycheckSkatt01 = new CalculatPaycheckSkatt();
             mycalculatPaycheckSkatt01.CalculatPaycheckEfterSkatt(paycheckInnanskatt, skatt); 
